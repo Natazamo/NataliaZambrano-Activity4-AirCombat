75b9083b0dd66399c0e3f03a652c3d6c4114fe5f
Natalia Zambrano Mortigo <n.tzm41@gmail.com>
2018-10-06 18:00:11 -0500

    Natalia Zambrano Airplane

---
+++ Assets/Airplane/Airplane.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airplane : MonoBehaviour {
+
+    public float speed; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.Translate(0, 0, speed);
+
+        //Right
+        if (Input.GetKey(KeyCode.RightArrow))
+        { this.transform.Translate(Vector3.right); } 
+
+        //Left
+        if (Input.GetKey(KeyCode.LeftArrow))
+        { this.transform.Translate(Vector3.left); } 
+
+        //Up
+        if (Input.GetKey(KeyCode.UpArrow))
+        { this.transform.Translate(Vector3.up); } 
+
+        //Down
+        if (Input.GetKey(KeyCode.DownArrow))
+        { this.transform.Translate(Vector3.down); }
+
+        //Space 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           
+            GameObject newMissile = GameObject.Instantiate(Resources.Load("Bullets/Missile") as GameObject);
+
+            GameObject Shootpivot = GameObject.Find("Pivot Airplane/ShootPivot");
+
+            newMissile.transform.SetParent(Shootpivot.transform);
+
+            newMissile.transform.localPosition = Vector3.zero;
+
+            newMissile.transform.SetParent(null);
+
+         
+    	}
+}
+}
---
+++ Assets/Missile/Missile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour {
+
+    public float motionSpeed;
+
+    public float rotationSpeed; 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(0, 0, rotationSpeed);
+        this.transform.Translate(Vector3.forward * motionSpeed);
+
+	}
+
+    void OnCollisionEnter (Collision collision)
+    {
+        Debug.Log("Collision entre dos colliders"); 
+             
+             GameObject.Destroy(this.gameObject);
+
+        GameObject.Destroy(collision.collider.gameObject);
+    }
+}
